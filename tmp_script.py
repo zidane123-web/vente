3e5f435a12f86b4a33442f9ab3cd45cd26e3de0e
Kavali7 <georgesbusiness54@gmail.com>
2025-11-04 13:09:59 +0100

    ne garder que l ui

--- tmp_script.py
+++
@@ -1,6 +0,0 @@
-import pathlib, re
-text = pathlib.Path('index.html').read_text(encoding='utf-8')
-m = re.search(r"commande' \? '(.+?)' : '(.+?)'", text)
-if m:
-    for g in m.groups():
-        print(g.encode('unicode_escape').decode())
--- tmp_seq.py
+++
@@ -1,7 +0,0 @@
-import pathlib, collections, re
-text=pathlib.Path('index.html').read_text(encoding='utf-8')
-seqs = ['\xd4\xf6','\xd4\xc7','\xd4\xe5','\xd4\xa3','\xd4\xd7','\xd4\xe2','\xd4\xfb','\xd4\xeb','\xd4\xdc']
-for seq in seqs:
-    m = re.search('.{0,20}'+re.escape(seq)+'.{0,20}', text)
-    if m:
-        print(seq.encode('unicode_escape').decode(), '->', m.group(0).encode('unicode_escape').decode())
