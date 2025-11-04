088141c97d4d5381ba4befd8e1d4057ca7720f5d
Kavali7 <georgesbusiness54@gmail.com>
2025-11-04 13:09:59 +0100

    ne garder que l ui

--- tests/runAll.js
+++
@@ -1,6 +0,0 @@
-require('./saleFlow.test');
-require('./livraisonEncaissement.test.js');
-require('./commandeFlow.test.js');
-require('./livraisonPermissions.test.js');
-require('./clientSelection.test.js');
-
